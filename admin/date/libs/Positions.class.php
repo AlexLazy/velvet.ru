01ae5802ddd572864cf18af9e5aeba73453b8c83
MysterGoN <nickgran@yandex.ru>
2016-05-09 21:46:56 +0300

    fixed

--- admin/date/libs/Positions.class.php
+++ admin/date/libs/Positions.class.php
@@ -32,21 +32,21 @@ class Positions
             $id2 = $_GET[id] + $_GET['down'];
         }
         if(isset($_GET['partners_position']) && (isset($_GET['up']) || isset($_GET['down'])) && isset($_GET['id'])) {
-            $mysqli->query("UPDATE ?_partners SET partner_id = 99999999 WHERE partner_id = {$_GET['id']};
-                            UPDATE ?_partners SET partner_id = {$_GET['id']} WHERE partner_id = {$id2};
-                            UPDATE ?_partners SET partner_id = {$id2} WHERE partner_id = 99999999;");
+            $mysqli->query("UPDATE ?_partners SET partner_id = 99999999 WHERE partner_id = " . $_GET['id']);
+            $mysqli->query("UPDATE ?_partners SET partner_id = " . $_GET['id'] . " WHERE partner_id = " . $id2);
+            $mysqli->query("UPDATE ?_partners SET partner_id = " . $id2 . " WHERE partner_id = 99999999;");
             echo "<script>location.href = '?partners_position'</script>";
         }
         if(isset($_GET['masters_position']) && (isset($_GET['up']) || isset($_GET['down'])) && isset($_GET['id'])) {
-            $mysqli->query("UPDATE ?_masters SET master_id = 99999999 WHERE master_id = {$_GET['id']};
-                            UPDATE ?_masters SET master_id = {$_GET['id']} WHERE master_id = {$id2};
-                            UPDATE ?_masters SET master_id = {$id2} WHERE master_id = 99999999;");
+            $mysqli->query("UPDATE ?_masters SET master_id = 99999999 WHERE master_id = " . $_GET['id']);
+            $mysqli->query("UPDATE ?_masters SET master_id = " . $_GET['id'] . " WHERE master_id = " . $id2);
+            $mysqli->query("UPDATE ?_masters SET master_id = " . $id2 . " WHERE master_id = 99999999;");
             echo "<script>location.href = '?masters_position'</script>";
         }
         if(isset($_GET['posts_position']) && (isset($_GET['up']) || isset($_GET['down'])) && isset($_GET['id'])) {
             $mysqli->query("UPDATE ?_posts SET post_id = 99999999 WHERE post_id = " . $_GET['id']);
             $mysqli->query("UPDATE ?_posts SET post_id = " . $_GET['id'] . " WHERE post_id = " . $id2);
-            $mysqli->query("UPDATE ?_posts SET post_id = ".$id2 . " WHERE post_id = 99999999;");
+            $mysqli->query("UPDATE ?_posts SET post_id = " . $id2 . " WHERE post_id = 99999999;");
             echo "<script>location.href = '?posts_position'</script>";
         }
     }
